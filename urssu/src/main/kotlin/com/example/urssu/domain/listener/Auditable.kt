ca98ac9e97cc1d15dd0230286a0993333ad4c4f7
Dayz-new <sdhdream99@naver.com>
2022-11-03 12:05:55 +0900

    refactor : Auditing 수정

--- urssu/src/main/kotlin/com/example/urssu/domain/listener/Auditable.kt
+++
@@ -1,8 +0,0 @@
-package com.example.urssu.domain.listener
-
-import java.time.LocalDateTime
-
-interface Auditable {
-    fun setCreatedAt(createdAt: LocalDateTime?)
-    fun setUpdatedAt(updatedAt: LocalDateTime?)
-}
